3cef4b5c2c83a62a04e8f4717dbeb19031f04672
Thomas H Aylesworth <thomas.aylesworth@gmail.com>
2020-04-27 21:22:51 -0400

    Initial Commit

---
+++ DungeonCrawlTests/DungeonCrawlTests.swift
@@ -0,0 +1,34 @@
+//
+//  DungeonCrawlTests.swift
+//  DungeonCrawlTests
+//
+//  Created by Thomas Aylesworth on 4/27/20.
+//  Copyright © 2020 Bayou Games. All rights reserved.
+//
+
+import XCTest
+@testable import DungeonCrawl
+
+class DungeonCrawlTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
