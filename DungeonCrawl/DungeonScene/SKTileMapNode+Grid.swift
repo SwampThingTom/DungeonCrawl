cb73e7f36bf241dd35400159f6d88e535744896a
Thomas H Aylesworth <thomas.aylesworth@gmail.com>
2020-05-09 14:52:54 -0400

    Add extension to get direction between points

---
+++ DungeonCrawl/DungeonScene/SKTileMapNode+Grid.swift
@@ -0,0 +1,51 @@
+//
+//  SKTileMapNode+Grid.swift
+//  DungeonCrawl
+//
+//  Created by Thomas Aylesworth on 5/9/20.
+//  Copyright © 2020 Bayou Games. All rights reserved.
+//
+
+import SpriteKit
+
+protocol GridCellProviding {
+    func cell(for position: CGPoint) -> GridCell
+    func center(of cell: GridCell) -> CGPoint
+}
+
+extension SKTileMapNode: GridCellProviding {
+    
+    func cell(for position: CGPoint) -> GridCell {
+        let x = tileColumnIndex(fromPosition: position)
+        let y = tileRowIndex(fromPosition: position)
+        return GridCell(x: x, y: y)
+    }
+    
+    func center(of cell: GridCell) -> CGPoint {
+        return centerOfTile(atColumn: cell.x, row: cell.y)
+    }
+}
+
+extension Direction {
+    
+    static func direction(from origin: CGPoint, to destination: CGPoint) -> Direction? {
+        let angle = (destination - origin).angle
+        return direction(for: angle)
+    }
+    
+    static func direction(for angle: CGFloat) -> Direction? {
+        let degrees = angle.radiansToDegrees()
+        switch degrees {
+        case -45 ..< 45:
+            return .east
+        case 45 ..< 135:
+            return .south
+        case 135 ..< 180, -180 ..< -135:
+            return .west
+        case -135 ..< -45:
+            return .north
+        default:
+            return nil
+        }
+    }
+}
---
+++ DungeonCrawl/DungeonScene/__tests__/SKTileMapNode+GridTests.swift
@@ -0,0 +1,62 @@
+//
+//  SKTileMapNode+GridTests.swift
+//  DungeonCrawlTests
+//
+//  Created by Thomas Aylesworth on 5/9/20.
+//  Copyright © 2020 Bayou Games. All rights reserved.
+//
+
+@testable import DungeonCrawl
+
+import XCTest
+
+class SKTileMapNodeGridTests: XCTestCase {
+    
+    func testDirectionForAngle() throws {
+        let pi_4 = CGFloat.pi / CGFloat(4.0)
+        let tests = [
+            (expected: Direction.east,
+             minAngle: -pi_4,
+             maxAngle: pi_4 - CGFloat(0.001)),
+            (expected: Direction.south,
+             minAngle: pi_4,
+             maxAngle: pi_4 * CGFloat(3.0) - CGFloat(0.001)),
+            (expected: Direction.west,
+             minAngle: pi_4 * CGFloat(3.0),
+             maxAngle: -pi_4 * CGFloat(3.0) - CGFloat(0.001)),
+            (expected: Direction.north,
+             minAngle: -pi_4 * CGFloat(3.0),
+             maxAngle: -pi_4 - CGFloat(0.001)),
+            (expected: nil,
+             minAngle: -CGFloat.pi - CGFloat(0.001),
+             maxAngle: CGFloat.pi + CGFloat(0.001))
+        ]
+        
+        tests.forEach { (expected, minAngle, maxAngle) in
+            XCTAssertEqual(Direction.direction(for: minAngle), expected)
+            XCTAssertEqual(Direction.direction(for: maxAngle), expected)
+        }
+    }
+    
+    func testDirectionFromPointToPoint() throws {
+        let origin = CGPoint(x: 0, y: 0)
+        let tests = [
+            (expected: Direction.east,
+             from: origin,
+             to: CGPoint(x: 120, y: 10)),
+            (expected: Direction.north,
+             from: origin,
+             to: CGPoint(x: -10, y: -120)),
+            (expected: Direction.west,
+             from: origin,
+             to: CGPoint(x: -120, y: 10)),
+            (expected: Direction.south,
+             from: origin,
+             to: CGPoint(x: -10, y: 120))
+        ]
+        
+        tests.forEach { (expected, from, to) in
+            XCTAssertEqual(Direction.direction(from: from, to: to), expected)
+        }
+    }
+}
