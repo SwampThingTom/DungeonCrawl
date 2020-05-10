9496b968a0181646021696dbe6de72705c05dabd
Thomas H Aylesworth <thomas.aylesworth@gmail.com>
2020-05-10 13:59:31 -0400

    Add enemies to DungeonDecorator

--- DungeonCrawl/DungeonScene/DungeonDecorator.swift
+++ DungeonCrawl/DungeonScene/DungeonDecorator.swift
@@ -12,13 +12,20 @@ protocol DungeonDecorating {
     func decorate(dungeon: DungeonModel) -> DungeonDecorations
 }
 
-struct DungeonDecorator: DungeonDecorating {
+class DungeonDecorator: DungeonDecorating {
+    
+    private var randomNumberGenerator: AnyRandomNumberGenerator
+    
+    init(randomNumberGenerator: RandomNumberGenerator = SystemRandomNumberGenerator()) {
+        self.randomNumberGenerator = AnyRandomNumberGenerator(randomNumberGenerator)
+    }
     
     func decorate(dungeon: DungeonModel) -> DungeonDecorations {
         guard let playerStartCell = playerStartCell(in: dungeon) else {
             fatalError("Unable to place player in dungeon")
         }
-        return DungeonDecorations(playerStartCell: playerStartCell, enemies: [])
+        let enemies = spawnEnemies(in: dungeon)
+        return DungeonDecorations(playerStartCell: playerStartCell, enemies: enemies)
     }
 
     private func playerStartCell(in dungeon: DungeonModel) -> GridCell? {
@@ -31,5 +38,24 @@ struct DungeonDecorator: DungeonDecorating {
             }
         }
         return nil
+    }
+    
+    private func spawnEnemies(in dungeon: DungeonModel) -> [EnemyModel] {
+        guard dungeon.rooms.count > 0 else {
+            return []
+        }
+        let room = dungeon.rooms.randomElement(using: &randomNumberGenerator)!
+        let cell = room.bounds.randomCell(using: &randomNumberGenerator)
+        let enemy = EnemyModel(spriteName: "Ghost", cell: cell)
+        return [enemy]
+    }
+}
+
+extension GridRect {
+    
+    func randomCell(using randomNumberGenerator: inout AnyRandomNumberGenerator) -> GridCell {
+        let x = gridXRange.randomElement(using: &randomNumberGenerator)!
+        let y = gridYRange.randomElement(using: &randomNumberGenerator)!
+        return GridCell(x: x, y: y)
     }
 }
--- DungeonCrawl/DungeonScene/__tests__/DungeonDecoratorTests.swift
+++ DungeonCrawl/DungeonScene/__tests__/DungeonDecoratorTests.swift
@@ -14,6 +14,20 @@ class DungeonDecoratorTests: XCTestCase {
 
     func testDecorate() throws {
         // Arrange
+        let dungeon = DungeonModel(map: fiveRegionMap(), rooms: threeRooms())
+        let sut = DungeonDecorator()
+        
+        // Act
+        let decorations = sut.decorate(dungeon: dungeon)
+        
+        // Assert
+        let tileAtStartCell = dungeon.map.tile(at: decorations.playerStartCell)
+        XCTAssertEqual(tileAtStartCell, .floor)
+        XCTAssertEqual(decorations.enemies.count, 1)
+    }
+    
+    func testDecorate_noRooms() throws {
+        // Arrange
         let dungeon = DungeonModel(map: fiveRegionMap(), rooms: [])
         let sut = DungeonDecorator()
         
@@ -23,7 +37,16 @@ class DungeonDecoratorTests: XCTestCase {
         // Assert
         let tileAtStartCell = dungeon.map.tile(at: decorations.playerStartCell)
         XCTAssertEqual(tileAtStartCell, .floor)
+        XCTAssertEqual(decorations.enemies.count, 0)
     }
+}
+
+private func threeRooms() -> [RoomModel] {
+     return [
+        RoomModel(bounds: GridRect(x: 1, y: 9, width: 7, height: 3)),
+        RoomModel(bounds: GridRect(x: 7, y: 1, width: 3, height: 7)),
+        RoomModel(bounds: GridRect(x: 13, y: 7, width: 7, height: 3))
+    ]
 }
 
 /// Returns a 17x15 map with five regions.
