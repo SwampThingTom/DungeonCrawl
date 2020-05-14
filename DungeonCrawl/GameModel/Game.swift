c8b862c102a6acb3019d02b4a55893a2553e6cb9
Thomas H Aylesworth <thomas.aylesworth@gmail.com>
2020-05-14 08:17:44 -0400

    Add Game model classes for managing state

---
+++ DungeonCrawl/GameModel/Game.swift
@@ -0,0 +1,13 @@
+//
+//  Game.swift
+//  DungeonCrawl
+//
+//  Created by Thomas Aylesworth on 5/14/20.
+//  Copyright © 2020 Thomas H Aylesworth. All rights reserved.
+//
+
+import Foundation
+
+class Game {
+    var level: LevelProviding?
+}
---
+++ DungeonCrawl/GameModel/LevelProviding.swift
@@ -0,0 +1,41 @@
+//
+//  LevelProviding.swift
+//  DungeonCrawl
+//
+//  Created by Thomas Aylesworth on 5/10/20.
+//  Copyright © 2020 Thomas H Aylesworth. All rights reserved.
+//
+
+import SpriteKit
+
+protocol LevelProviding {
+    var map: GridMap { get }
+    var player: Actor { get }
+    var actors: [AIActor] { get }
+}
+
+protocol Actor {
+    var name: String { get }
+    var cell: GridCell { get }
+    func doTurnAction(_ action: TurnAction) -> Animation?
+}
+
+protocol AIActor: Actor {
+    var enemyType: EnemyType { get }
+    func turnAction(level: LevelProviding) -> TurnAction
+}
+
+enum TurnAction {
+    case attack(direction: Direction)
+    case move(to: GridCell, direction: Direction)
+    case nothing
+}
+
+enum Animation {
+    case attack(heading: Direction)
+    case move(to: GridCell, heading: Direction)
+}
+
+enum EnemyType {
+    
+}
