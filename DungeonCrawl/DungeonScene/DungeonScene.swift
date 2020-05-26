be692b0c36555535c926ca9e18ab5f2f7fe3c1c4
Thomas H Aylesworth <thomas.aylesworth@gmail.com>
2020-05-26 09:22:18 -0400

    Use lowercase for unequipped item labels

--- DungeonCrawl/DungeonScene/DungeonScene.swift
+++ DungeonCrawl/DungeonScene/DungeonScene.swift
@@ -107,9 +107,9 @@ extension DungeonScene: HUDDelegate {
         let player = game.level.player
         if let combat = player.combatComponent() {
             hudView?.healthLabel?.text = "\(combat.hitPoints) / \(combat.maxHitPoints)"
-            let armorName = player.inventoryComponent()?.equippedItem(for: .armor)?.item.name ?? "None"
+            let armorName = player.inventoryComponent()?.equippedItem(for: .armor)?.item.name ?? "none"
             hudView?.armorLabel?.text = "\(armorName) (\(combat.armorClass))"
-            let weaponName = player.inventoryComponent()?.equippedItem(for: .weapon)?.item.name ?? "Unarmed"
+            let weaponName = player.inventoryComponent()?.equippedItem(for: .weapon)?.item.name ?? "unarmed"
             hudView?.weaponLabel?.text = weaponName
         }
         if let items = player.inventoryComponent() {
